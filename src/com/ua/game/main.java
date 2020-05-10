5f29568b1afb784e477cc7c7d263ccc58918751f
OLEG <margorskyoleg@gmail.com>
2020-05-10 03:21:06 +0200

    Merge branch 'master' into B4

--- src/com/ua/game/main.java
+++ src/com/ua/game/main.java
@@ -2,14 +2,8 @@ package com.ua.game;
 
 public class main {
     public static void main(String[] args) {
-//1
-//2
-//3
-//4
-//5
-//6
-//7
-//8
-////
+        
+    }
+    private static void ui(){
     }
 }
