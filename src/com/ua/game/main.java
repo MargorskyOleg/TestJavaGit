a7202cb51b7feb4a07a7b2ecf0316332d4628e5f
Margorsky Oleg <42284596+MargorskyOleg@users.noreply.github.com>
2020-05-10 03:29:12 +0200

    UP
    5

--- src/com/ua/game/main.java
+++ src/com/ua/game/main.java
@@ -1,11 +1,13 @@
 package com.ua.game;
 
 public class main {
+    
     public static void main(String[] args) {
-
+        uir();
     }
+    
     private static void uir(){
-
+        uir22();
     }
 
     private static void uir22(){
